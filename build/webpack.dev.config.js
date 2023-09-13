31e40b753fc6ecb4b85f7c81a6c70a9bff7239f6
pupu <pupu@pupudeMac-mini.local>
2023-09-13 15:30:20 +0800

    feat: webpack构建优化

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,22 +1,22 @@
-const baseConfig=require('./webpack.base.config');
-const {merge} =require('webpack-merge') 
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
-const path= require('path')
-const isDev=process.env.NODE_ENV==='development'
+const baseConfig=require("./webpack.base.config");
+const {merge} =require("webpack-merge"); 
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const path= require("path");
+const isDev=process.env.NODE_ENV==="development";
 module.exports = merge(baseConfig,{
-    mode:'development',
-    devtool:'eval-cheap-module-source-map',
-    devServer:{
-        port:3000,
-        compress:false,
-        hot:true,
-        historyApiFallback:true,
-        //热更时不打包静态资源
-        static:{
-            directory : path.join(__dirname,'../public')
-        }
-    },
-    plugins:[
-        new ReactRefreshWebpackPlugin()
-    ]
-})
+	mode:"development",
+	devtool:"eval-cheap-module-source-map",
+	devServer:{
+		port:10086,
+		compress:false,
+		hot:true,
+		historyApiFallback:true,
+		//热更时不打包静态资源
+		static:{
+			directory : path.join(__dirname,"../public")
+		}
+	},
+	plugins:[
+		new ReactRefreshWebpackPlugin()
+	]
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import styles from './app.module.less'
+import styles from "./app.module.less";
 const App = () => {
-    console.log(styles)
-    // issue:获得的类名为乱码
-    let [inputValue, setInputValue] = useState<string>("")
-    return <div className={styles.container}>
-        <h2>hhh</h2>
+	console.log(styles);
+	// issue:获得的类名为乱码
+	const [inputValue, setInputValue] = useState<string>("");
+	return <div className={styles.container}>
+		<h2>hhh</h2>
         Hey,World
-        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-        <p>{inputValue}</p>
-    </div>
-}
+		<input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+		<p>{inputValue}</p>
+	</div>;
+};
 export default App;
